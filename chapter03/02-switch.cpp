76791ab12737aad462bdbc3a13d3d16678c1e0f5
Murray Crane <murray.crane@gmail.com>
2024-12-13 12:30:52 +0000

    Create 02-switch.cpp
    Chapter 03 exercise 02.

---
+++ chapter03/02-switch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num=6;
+
+    switch (num)
+    {
+        case 1: cout << num << " : Monday"; break;
+        case 2: cout << num << " : Tuesday"; break;
+        case 3: cout << num << " : Wednesday"; break;
+        case 4: cout << num << " : Thurday"; break;
+        case 5: cout << num << " : Friday"; break;
+        default: cout << num << " : Weekend day";
+    }
+    return 0;
+}
